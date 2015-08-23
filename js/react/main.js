fd19852f651bb60651d21d31a87c94fe6c708880
Matej Janacek <majana@microsoft.com>
2015-08-23 20:56:29 +0200

    Adding fallbacks to CDN libraries, using require lib included in the project

--- js/react/main.js
+++ js/react/main.js
@@ -3,8 +3,8 @@
 requirejs.config({
     baseUrl: 'js/react',
     paths: {
-        'react': 'https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/react',
-        'WindowsAzure': 'http://ajax.aspnetcdn.com/ajax/mobileservices/MobileServices.Web-1.2.5'
+        'react': ['https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/react', 'vendor/react.min'],
+        'WindowsAzure': ['http://ajax.aspnetcdn.com/ajax/mobileservices/MobileServices.Web-1.2.5', 'vendor/mobileServices.min']
     }
 });
 
